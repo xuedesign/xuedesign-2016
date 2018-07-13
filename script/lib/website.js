5d3b00284632d74a72b6dc3d21aa4b02714ad4a7
xuedesign <elmo@xuedesign.tw>
2018-07-13 22:45:02 +0800

    首頁導入LabJS測試 ＃20180713

---
+++ script/lib/website.js
@@ -0,0 +1,53 @@
+// 另開視窗
+function externalLinks() { 
+ if (!document.getElementsByTagName) return; 
+ var anchors = document.getElementsByTagName("a"); 
+ for (var i=0; i<anchors.length; i++) { 
+   var anchor = anchors[i]; 
+   if (anchor.getAttribute("href") && 
+       anchor.getAttribute("rel") == "external") 
+     anchor.target = "_blank"; 
+ } 
+} 
+window.onload = externalLinks;
+
+//圖片延遲載入
+$(function() {
+    $("img").lazyload({placeholder : "../images/grey.gif",effect: "fadeIn"});
+      });
+	  
+//返回頁首
+$(function(){
+  $("#top-menu").scrollTo(700)
+});
+$(function(){
+  $("#mouse-move").scrollTo(700)
+});
+$(function(){
+  $("#btn-first").scrollTo(700)
+});
+
+
+//頂端隱藏選單
+$(function(){
+	$(window).load(function(){
+		$(window).bind('scroll resize', function(){
+			var $this = $(this);
+			var $this_Top=$this.scrollTop();
+			
+			//當高度小於100時，關閉區塊	
+			if($this_Top < 150){
+				$('#top-bar').stop().animate({top:"-65px"});
+				}
+			if($this_Top > 150){
+				$('#top-bar').stop().animate({top:"0px"});
+						
+			}
+		}).scroll();
+	});
+});
+
+
+//wow動畫
+wow=new WOW({animateClass:"animated",offset:100,callback:function(a){console.log("WOW: animating <"+a.tagName.toLowerCase()+">")}}),wow.init();
+
