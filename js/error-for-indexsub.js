8ad72f8192a26a59a86566ceb3f30c2a8929cb5b
ElmoWoo <hello@xuedesign.tw>
2021-05-24 18:20:59 +0800

    啟用SCSS與JS壓縮 #20210524

---
+++ js/error-for-indexsub.js
@@ -0,0 +1,39 @@
+//justfont
+loadjs('../../script/lib/justfonts.js');
+
+//icon文字
+loadjs('https://use.fontawesome.com/7b7fd6d88b.js');
+
+//wow載入動畫
+loadjs('../../script/lib/wow.min.js', function() {
+  new WOW().init();
+});
+
+//圖片延遲載入
+loadjs('https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js', function() {
+    const observer = lozad();
+    observer.observe();
+});
+
+//chatcode
+loadjs('../../script/lib/chatcode.js');
+
+//頂端隱藏選單
+$(function(){
+	$(window).load(function(){
+		$(window).bind('scroll resize', function(){
+			var $this = $(this);
+			var $this_Top=$this.scrollTop();
+			
+			//當高度小於100時，關閉區塊	
+			if($this_Top < 150){
+				$('#top-bar').stop().animate({top:"-65px"});
+				}
+			if($this_Top > 150){
+				$('#top-bar').stop().animate({top:"0px"});
+						
+			}
+		}).scroll();
+	});
+});
+
